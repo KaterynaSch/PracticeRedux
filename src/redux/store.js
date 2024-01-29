e17856550c1cff34dd6e2d6ee3f62e1e676f29ff
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2024-01-29 18:37:36 +0200

    middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,46 @@ import { configureStore } from "@reduxjs/toolkit";
 import { accountReducer } from "./accountSlice";
 import { localeReducer } from "./localeSlice";
 
+//оолошення middleware - це функція яка повертає функцію, яка повертає ф-цію роботи з action
+// const myMiddleware1 =store => next => action => {console.log("myMiddleware1: ", action); next(action)};        
+
+// const myMiddleware1 = store => {//коли екшен потр. в редюсер, то відловлюється цим мідлваром
+//     return next => {// next- ф-ція для відправки екшна далі по ланцюжку
+//         return action => {
+//             console.log("myMiddleware1: ", action);
+//             //myMiddleware1:  {type: 'locale/changeLang', payload: 'pl'}payload: "pl"type: "locale/changeLang"[[Prototype]]: Object
+//             // myMiddleware1:  {type: 'account/deposit', payload: 1}
+//             // myMiddleware1:  {type: 'account/withdraw', payload: 1}
+//             next(action);//посилання на наступний middleware, якшо немає наст мідлвари ,то виклик редюсери
+//         };
+//     };
+// };
+
+// const myMiddleware2 =store => next => action => {console.log("myMiddleware2: ", action); next(action);//якшо немає наст. мідлвари ,то виклик редюсери           
+// };
+
+
 export const store = configureStore({//сам створює кореневий редюсер
     reducer: {
         account: accountReducer,
         locale: localeReducer,
     },
+    // middleware(getDefaultMiddleware){
+    //     const defaultMiddleware = getDefaultMiddleware();
+    //     return [...defaultMiddleware, myMiddleware1, myMiddleware2];
+
+    // }
 });
+
+// // ця middleware реагує на тип екшена
+// //  первіряє якщо action не об'єкт, а ф-ція ,то він її викликає і передає в dispatch і не пропускає далі
+// const reduxThunkMd = store => next => action => {// робота з асинхроном,виконання HTTP- запитів
+//     if (typeof action !== 'function') {
+//         next(action);
+//         return;
+//     }
+//     action(store.dispatch);
+// };
 
 // const rootReducer = combineReducers({
 //     account: accountReducer,
